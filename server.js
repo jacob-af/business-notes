53047b4ca7d6b0c2481bdffd515ac91924409cd5
Jacob Feitler <jacobfeitler@Jacobs-MacBook-Pro.local>
2021-02-24 12:15:20 -0500

    fixed order of routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // Serves JS and CSS files from the public directory
 app.use(express.static(__dirname + "/public"));
 
-require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
