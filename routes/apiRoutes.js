8f451cdc0ba892b46b2e88b9f35626a8a21a63e0
Jacob Feitler <jacobfeitler@Jacobs-MacBook-Pro.local>
2021-02-24 22:57:29 -0500

    playing with require

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
+const data = require("../db/db.json")
+const uuid = require("uuid")
 
 module.exports = (app) => {
 
   // Route: GET api/notes
   // Desc: returns contents of db.json
   app.get("/api/notes", (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db/db.json"));
     res.send(data);
   });
 
@@ -13,9 +14,8 @@ module.exports = (app) => {
   // Desc: adds new note to array of notes,
   // updates db.json and returns new array
   app.post("/api/notes", (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db/db.json"));
     // gives new note a random unique id
-    req.body.id = Math.random().toString(36).substr(2, 16);
+    req.body.id = uuid.v4();
     // adds note to array
     data.push(req.body);
     //update the db.json with new array
@@ -30,12 +30,11 @@ module.exports = (app) => {
   // Desc: removes note with selected id from array
   // updates db.json and returns new array
   app.delete("/api/notes/:id", (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./db/db.json"));
     // finds index of note with selected id
     let idToRemove = data.findIndex((ele) => ele.id === req.params.id);
     // removes appropriate index
     data.splice(idToRemove, 1);
-    
+
     fs.writeFileSync("./db/db.json", JSON.stringify(data), (err) =>
       err ? console.log(err) : console.log("success")
     );
